2ec057c83f78bd6d809f4c0e460b4ea0a6767882
Renanvt <jonatan_rvs@hotmail.com>
2020-10-18 14:26:43 -0300

    Criando projeto com nodejs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,20 @@
 import express from 'express'; //Importando o express
 const app = express();
 
-/*get(rota,funcao)*/ 
+/*get(rota,funcao)
+//request -> dados do usuario, nome, email, senha, parametros
+//response -> tipo de resposta, status, error
+//Rota = conjunto
+//Recurso = usuario
+//Métodos HTTP = GET, POST, PUT, DELETE
+//Parâmetros
+//GET = BUSCAR UMA INFORMAÇÃO
+*/ 
 
-app.get('/users', () => {
-    console.log('teste');
+//ROTA:
+app.get('/users', (request, response) => {
+    //return response.send('Hello World!');
+    return response.json({ message: 'Hello World!' });
 });
 
 app.listen(3333); //Ouvir a porta
